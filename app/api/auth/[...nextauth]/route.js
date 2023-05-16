2beaa54a1f653ef0ef07525a8efd3c9b54b296a0
kumailnxtdev <komailnaqvi616@gmail.com>
2023-05-16 19:10:35 +0500

    sign in function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,29 +1,36 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
-    providers :[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    async session({session}){
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+      // check if user already exists
+      const userExist = await User.findOne({ email: profile.email });
+      // if not create a new user
+      if (!userExist) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
-    },
-    async signIn({profile}){
-        try {
-            await connectToDB();
-            // check if user already exists
-            // if not create a new user
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-})
+  },
+});
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
