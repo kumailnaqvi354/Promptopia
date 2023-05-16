0923c3b8abd8a72e82e5d3cd7cb59bebb5d0ad90
kumailnxtdev <komailnaqvi616@gmail.com>
2023-05-16 19:12:34 +0500

    user session function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,14 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+        email: session.user.email
+    })
+    session.user.id = session._id.toString();
+    return session;
+
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
