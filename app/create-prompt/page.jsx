4c03275a4dc09d7022172338cc52397fd4571fd7
kumailnxtdev <komailnaqvi616@gmail.com>
2023-05-22 19:38:52 +0500

    update

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+"use client";
+import Form from '@components/Form';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 function CreatePrompt() {
-  return (
-    <div>CreatePrompt</div>
+const [submitting, setSubmitting] = useState(false);
+const [post, setPost] = useState({
+    prompt: "",
+    tag: ""
+});
+
+const create = async () =>{
+     
+}
+return (
+<Form 
+
+/>
   )
 }
 
