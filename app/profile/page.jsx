c645c31f08897e64989b46ddf5a3f95c119639b7
kumailnxtdev <komailnaqvi616@gmail.com>
2023-05-25 19:22:18 +0500

    profile page added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import Profile from "@components/profile";
 
-function Profile() {
+function MyProfile() {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
   return (
-    <div>Profile</div>
-  )
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 }
 
-export default Profile
+export default MyProfile;
